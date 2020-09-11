813fb1acabbe2648db556b312526a52192e2dca6
ArtemKez <artem.kez.work@gmail.com>
2020-09-11 11:58:18 +0300

    10

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
+  render() {
+    return (
+        <button
+            className="square"
+            onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
+        </button>
+    );
   }
+}
   render() {
     return (
         <button
